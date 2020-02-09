434c148365023e0375358d1c850abf5387c23f34
Amélie Antigny <amelie.antigny@free.fr>
2020-02-09 20:06:03 +0100

    Tp2 donc formulaire ajout et modif mais le modif ne marche pas

--- ProStages/src/Controller/ProStagesController.php
+++ ProStages/src/Controller/ProStagesController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response; 
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\HttpFoundation\Request; 
+use Doctrine\Common\Persistence\ObjectManager;
 
 use App\Entity\Stage;
 use App\Entity\Entreprise;
@@ -101,8 +105,67 @@ class ProStagesController extends AbstractController
 
     }
 
+    /**
+     * @Route("/ajoutEntreprise", name="ProStages-AjoutEntreprise")
+     */
+    public function ajoutEntreprise(Request $request, ObjectManager $manager)
+    {
+
+        //creation d'une ressource vierge qui sera remplie par le formulaire
+        $entreprise = new Entreprise();
+
+        //affiche la page d'ajout d'entreprise
+        $formulaireEntreprise = $this->createFormBuilder($entreprise)
+        ->add('nom')
+        ->add('activiter', TextareaType::class)
+        ->add('adresse')
+        ->add('lienSiteWeb', UrlType::class)
+        ->getForm();
+       
+        //on demande d'analyser la derniere requete http, 
+        //recupere et affecte les donner recup au nouveau objet
+        $formulaireEntreprise->handleRequest($request);
+
+        if($formulaireEntreprise->isSubmitted()){
+            $manager->persist($entreprise);
+            $manager->flush();
+            return $this->redirectToRoute('ProStages-Accueil');
+        }
+        
+    //envoyer les formulaires récuperer à la vue charger de las afficher 
+        return $this->render('ProStages/ajoutModifEntreprise.html.twig', ['vueFormulaire' =>  $formulaireEntreprise->createView(),'action'=>"ajouter"]);
+
+    }
 
 
+    /**
+     * @Route("/modifierEntreprise/{$id}", name="ProStages-ModifierEntreprise")
+     */
+    public function modifierEntreprise(Request $request, ObjectManager $manager, Entreprise $entreprise)
+    {
+        //affiche la page d'ajout d'entreprise
+        $formulaireEntreprise = $this->createFormBuilder($entreprise)
+        ->add('nom')
+        ->add('activiter', TextareaType::class)
+        ->add('adresse')
+        ->add('lienSiteWeb', UrlType::class)
+        ->getForm();
+       
+        //on demande d'analyser la derniere requete http, 
+        //recupere et affecte les donner recup au nouveau objet
+        $formulaireEntreprise->handleRequest($request);
 
+        if($formulaireEntreprise->isSubmitted()){
+            $manager->persist($entreprise);
+            $manager->flush();
+            return $this->redirectToRoute('ProStages-Accueil');
+        }
+        
+    //envoyer les formulaires récuperer à la vue charger de las afficher 
+        return $this->render('ProStages/ajoutModifEntreprise.html.twig', ['vueFormulaire' =>  $formulaireEntreprise->createView(),'action'=>"modifier" ] );
+
+    }
 
 }
+
+   
