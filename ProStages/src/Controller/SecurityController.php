bb1d9eac0552cb84568cd42fbb578c1d6a5b0a42
Amélie Antigny <amelie.antigny@free.fr>
2020-03-09 19:37:16 +0100

    création formulaire d'inscription d'un nouvel utilisateur donc TP5 OK

--- ProStages/src/Controller/SecurityController.php
+++ ProStages/src/Controller/SecurityController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -32,5 +37,38 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+
+     /**
+     * @Route("/ajoutUser", name="app_ajoutUser")
+     */
+    public function ajoutUser(Request $request, ObjectManager $manager,  UserPasswordEncoderInterface $encoder)
+    {
+        //creation d'une ressource vierge qui sera remplie par le formulaire
+        $user = new User();
+
+        //affiche la page d'ajout d'entreprise
+        $formulaireUser = $this->createForm(UserType::class, $user);
+    
+        //on demande d'analyser la derniere requete http, 
+        //recupere et affecte les donner recup au nouveau objet
+        $formulaireUser->handleRequest($request);
+
+        if($formulaireUser->isSubmitted() && $formulaireUser->isValid()){
+            $user->setRoles(['ROLE_USER']);
+
+            //Encoder le mot de passe de l'utilisateur
+            $encodagePassword = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($encodagePassword);
+
+            // Enregistrer l'utilisateur en base de données
+            $manager->persist($user);
+            $manager->flush();
+
+            // Rediriger l'utilisateur vers la page de login
+            return $this->redirectToRoute('ProStages-Accueil');
+        }
+        //envoyer les formulaires récuperer à la vue charger de las afficher 
+        return $this->render('ProStages/ajoutUser.html.twig',['vueFormulaireUser' => $formulaireUser->createView()]);
     }
 }
